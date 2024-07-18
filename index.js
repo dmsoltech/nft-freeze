fc653c636dd615e9ba5208cc80676845fc5a8151
unknown <testmestforbest@mailer.com>
2024-07-18 17:28:01 -0400

    fix

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ import {
     mplTokenMetadata,
     lockV1,
   } from '@metaplex-foundation/mpl-token-metadata';
-import * as bs58 from "bs58";
+import bs58 from "bs58";
 import { keypairIdentity } from '@metaplex-foundation/umi';
 import { PublicKey } from '@solana/web3.js';
-import { createUmi } from '@metaplex-foundation/umi';
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
 
 export const privatekey = "your private key here"
 export const nftMint = "nft mint address here"
+export const endpoint = "https://api.mainnet-beta.solana.com"
 
 async function main() {
-    const umi = createUmi().use(mplTokenMetadata());
+    const umi = createUmi(endpoint).use(mplTokenMetadata());
     
     const keypair = umi.eddsa.createKeypairFromSecretKey(bs58.decode(privatekey))
 
